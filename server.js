26822a74670ab60b99ec9be5aca57e03f954d3b2
ced_stark08 <cedricngangue08@gmail.com>
2023-08-03 00:03:08 +0200

    api done4

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Library API",
+      title: "TCF API",
       version: "1.0.0",
-      description: "A simple Express Library API",
+      description: "documentation of tcf web service",
     },
     components: {
       securitySchemes: {

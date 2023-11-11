94a19e33a7b641ddbe54c983af733ede559205f1
ced_stark08 <cedricngangue08@gmail.com>
2023-11-11 12:18:23 +0100

    problem resolve

--- server.js
+++ server.js
@@ -96,18 +96,18 @@ app.use("/api/eeQuestion", require("./routes/api/questionEERoute"));
 app.use("/api/eoQuestion", require("./routes/api/questionEORoute"));
 app.use("/api/eeTest", require("./routes/api/testEERoute"));
 app.use("/api/eoTest", require("./routes/api/testEORoute"));
-app.use("/api", require("./routes/api/paymentRoute"));
+app.use("/api", require("./routes/api/paymentRoute"))
 
 app.all("/*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
+    res.sendFile(path.join(__dirname, "views", "404.html"))
   } else if (req.accepts("json")) {
-    res.json({ error: "404 not found" });
+    res.json({ error: "404 not found" })
   } else {
-    res.type("txt").send("404 not found");
+    res.type("txt").send("404 not found")
   }
-});
+})
 
 app.use(errorHandle);
 mongoose.connection.once('open', ()=> {

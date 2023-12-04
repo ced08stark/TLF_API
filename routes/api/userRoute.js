d587231ad2d6f89cccad60449487086170a8bc91
ced_stark08 <cedricngangue08@gmail.com>
2023-12-04 21:22:51 +0100

    error fix

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -266,16 +266,16 @@ const paymentController = require("../../controllers/paymentController");
 
 
 
-router
-  .route("/users/activeAccount")
-  .post(verifyJWT, paymentController.activeUserAccount);
+
 router.route("/check-subscrire").get(verifyJWT, userController.checkRemain);
 router.route("/users/user-info").get(verifyJWT, userController.getCurrentUser);
 router.route("/users").get(verifyJWT, userController.getUsers);
 router.route("/users/:id").get(verifyJWT, userController.getUser);
 router.route("/users/:id").delete(verifyJWT, userController.deleteUser);
 router.route("/users/:id").patch(verifyJWT, userController.updateUser);
-
+router
+  .route("/users/activeAccount")
+  .post(verifyJWT, paymentController.activeUserAccount);
 
 
 module.exports = router;
